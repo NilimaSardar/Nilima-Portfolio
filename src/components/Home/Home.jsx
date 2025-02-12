33a68a54aa54c4ca8fb8f1dfa3ab31c268be56e2
NilimaSardar <snilima181@gmail.com>
2025-02-12 09:17:40 +0545

    worked on css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
-import Button from './Button';
-import { MdOutlineFileDownload } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaAngleDown } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <HomePage>
         <div className='main-section'>
             <div className='intro'>
+                <h1>&lt; Nilima <br /> Sardar /&gt;</h1>
+                <span>Frontend Developer</span>
+                <ul>
+                  <Link to='/'><li><FaGithub/></li></Link>
+                  <Link to='/'><li><FaLinkedin/></li></Link>
+                  <Link to='/'><li><FaSquareInstagram/></li></Link>
+                </ul>
+            </div>
+            {/* <div className='intro'>
                 <h1>Hi! I'm </h1>
                 <span>NILIMA</span>
                 <h2>Frontend Developer</h2>
@@ -15,11 +27,17 @@ function Home() {
                   <Button color='color' text='Download CV' icon={<MdOutlineFileDownload fontSize='max(1.5vw, 13px)'/>}/>
                   <Button text='About Me'/>
                 </div>
-            </div>
+            </div> */}
             
             <div className='image'>
                 <img src="https://static.vecteezy.com/system/resources/previews/010/865/005/non_2x/continuous-line-drawing-woman-sitting-holding-laptop-vector.jpg" alt="" />
             </div>
+        </div>
+        
+        <div className="down">
+          <div className="down-icon">
+            <FaAngleDown/>
+          </div>    
         </div>
     </HomePage>
   )
@@ -28,15 +46,14 @@ function Home() {
 export default Home
 
 const HomePage = styled.div`
-    /* width: 100%; */
-    /* background-color: rebeccapurple; */
 
       .main-section{
         font-size: 35px;
         display: flex;
-        justify-content: space-between;
+        justify-content: space-around;
+        align-items: center;
         /* background-color: red; */
-        padding: 100px;
+        padding: 150px;
         height: calc(100vh - 80px);
 
       }
@@ -44,22 +61,32 @@ const HomePage = styled.div`
         line-height: 50px;
         padding: 100px 0;
       }
+      .intro ul{
+        width: 100%;
+        display: flex;
+        list-style-type: none;
+        gap: 15px;
+        padding-left: 20px;
+      }
+      .intro ul li{
+        font-size: 28px;
+        color: #5e5d5d9f;
+        transition: 0.3s;
+      }
+      .intro ul li:hover{
+        color: #5e5d5d;
+      }
       
       .intro span{
         font-weight: 700;
         letter-spacing: 5px;
       }
-      .intro h2{
+      .intro h1{
         font-size: max(4vw, 32px);
       }
-      .intro .button{
-        /* background-color: red; */
-        margin: 20px 10px;
-        display: flex;
-        gap: 10px;
-      }
+
       .image{
-        width: 50%;
+        width: 45%;
         height: auto;
       }
       .image img{
@@ -69,4 +96,34 @@ const HomePage = styled.div`
         /* border: 1px solid blue; */
       }
 
+      .down{
+        width: 100%;
+        height: 80px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /* background-color: #5e5d5d; */
+      }
+
+      .down-icon{
+        /* background-color: azure; */
+        height: 80px;
+        width: 80px;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 38px;
+        color: #5e5d5d9f;
+        animation: bounce 1.5s infinite ease-in-out;
+      }
+
+      @keyframes bounce {
+          0%, 100% {
+              transform: translateY(0);
+          }
+          50% {
+              transform: translateY(-10px);
+          }
+      }
 `;
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -34,6 +34,10 @@ export default Projects
 const Section = styled.div`
   width: 100%;
   padding: 30px 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   
   h1{
     font-size: max(2vw, 24px);
@@ -43,12 +47,14 @@ const Section = styled.div`
 `;
 
 const ProjectSection = styled.div`
+    width: 100%;
+    max-width: 1200px;
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(350px,1fr));
-    /* grid-template-columns: repeat(1, minmax(0, 1fr)); */
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    justify-content: center;
+    align-items: center;
+    place-items: cente; 
     margin-top: 30px;
     gap: 30px;
     row-gap: 50px;
-
-    
 `;
